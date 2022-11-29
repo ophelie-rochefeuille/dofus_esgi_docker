ff0f0bac9359c0e1ee1844a7d05398d254fe5c46
Ophelie Rochefeuille <ophelie.rochefeuille@green-conseil.com>
2022-11-30 00:25:04 +0100

    Docker finished

--- iop-react/src/App.js
+++ iop-react/src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React from "react";
+
+const baseURL = "http://localhost:9999/grades";
+
 
 function App() {
+   /* const [notes, setNotes] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setNotes(response.data)
+        })
+    }) */
   return (
     <div className="App">
-      <p>Testing</p>
+      <h1>Docker Soutenance</h1>
+        <h3>Objectifs</h3>
+        <p>Création d'images avec Dockerfile pour Symfony / Express JS / React  ✔</p>
+        <p>Création et connexion à une BDD Mysql ✔</p>
+        <p>Connexion avec phpMyAdmin ✔</p>
+        <p>Utilisation de Traefik avec comme point d'entrée React ✔</p>
+        <p>Email utilisable depuis Symfony ✔</p>
+        <p>Utilisation du cache avec Symfony  <strong>?</strong></p>
+        <p>Utilisation de Traefik avec https ✘</p>
+        <p>Création d'images pour la MEP ✘</p>
+
+
     </div>
   );
 }
 
 export default App;
+
--- steamer-symfony/src/Controller/GradesController.php
+++ steamer-symfony/src/Controller/GradesController.php
@@ -7,14 +7,16 @@ use App\Repository\GradesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Contracts\Cache\CacheInterface;
 class GradesController extends AbstractController
 {
     #[Route('/grades', name: 'app_grades')]
-    public function index(GradesRepository $gradesRepository): Response
+    public function index(GradesRepository $gradesRepository, CacheInterface $cache): Response
     {
+        dump($cache);
         return $this->render('grades/index.html.twig', [
             'grades' => $gradesRepository->findAll(),
         ]);
     }
+
 }
